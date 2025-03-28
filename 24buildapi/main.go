c826a8f0c3f5e3d666b0d1a2eb6dbc6e8097f6ae
Nishant <nishantraj839@gmail.com>
2025-03-28 18:07:53 +0530

    Sending API Response

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Model for course - file
 
@@ -19,13 +23,30 @@ type Author struct {
 // fake DB
 var courses []Course
 
-//middleware, helper - file
+// middleware, helper - file
 func (c *Course) IsEmpty() bool {
 	return c.CourseID == "" && c.CourseName == ""
 }
 
 func main() {
-
 	fmt.Println("Welcome to APIs in Golang !!")
 
+}
+
+// Controllers -- file
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("<h1> Welcome to API by learnCodeOnline !</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Get all courses")
+	// sets the response writer as content type and tells the client that can be web browser or postman that the file or response is sent in the json/format..
+	w.Header().Set("Content-Type", "application/json")
+	//Now the json format is now encoded to give you file in the form of json which we have created in fake db. This process is called as seeding.
+	json.NewEncoder(w).Encode(courses)
 }
